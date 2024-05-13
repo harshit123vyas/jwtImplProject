d2d3834ae59d88475782280d09f8b59d81ee617f
harshit123vyas <167099626+harshit123vyas@users.noreply.github.com>
2024-05-13 14:34:01 +0530

    Update RoleRepository.java

--- UserService/src/main/java/com/tasktracker/userservice/Repository/RoleRepository.java
+++ UserService/src/main/java/com/tasktracker/userservice/Repository/RoleRepository.java
@@ -3,7 +3,7 @@ package com.tasktracker.userservice.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tasktracker.userservice.Entity.Role;
-
+@Repository
 public interface RoleRepository  extends JpaRepository<Role, Integer>{
 	
 	Role findByRole(String role);
